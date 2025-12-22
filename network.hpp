6a2dbd988d6d04b080c86152f4711e8e3bba3f83
kavin kumar <kavinkumar2k1@gmail.com>
2025-12-22 23:09:47 +0100

    Implement Conv2d forward pass

--- network.hpp
+++ network.hpp
@@ -65,10 +65,74 @@ class Layer {
 
 
 class Conv2d : public Layer {
-    public:
-        Conv2d(size_t in_channels, size_t out_channels, size_t kernel_size, size_t stride=1, size_t pad=0) : Layer(LayerType::Conv2d) {}
-    // TODO
+public:
+    Conv2d(size_t in_channels,
+           size_t out_channels,
+           size_t kernel_size,
+           size_t stride = 1,
+           size_t pad = 0)        // this layer's constructor, so we can set specific attributes for different layer types
+        : Layer(LayerType::Conv2d),
+          in_channels_(in_channels),
+          out_channels_(out_channels),
+          kernel_size_(kernel_size),
+          stride_(stride),
+          pad_(pad)
+    {
+        weights_ = Tensor(out_channels_, in_channels_, kernel_size_, kernel_size_);
+        bias_    = Tensor(out_channels_);
+    }
+    // override the forward method
+    void fwd() override {
+        size_t N = input_.N;
+        size_t Cin = input_.C;
+        size_t Hin = input_.H;
+        size_t Win = input_.W;
+
+        size_t k = kernel_size_;
+
+        size_t Hout = (Hin + 2 * pad_ - k) / stride_ + 1;
+        size_t Wout = (Win + 2 * pad_ - k) / stride_ + 1;
+
+        output_ = Tensor(N, out_channels_, Hout, Wout);
+
+        // Perform convolution
+        for (size_t n = 0; n < N; ++n) {
+            for (size_t oc = 0; oc < out_channels_; ++oc) {
+                for (size_t oh = 0; oh < Hout; ++oh) {
+                    for (size_t ow = 0; ow < Wout; ++ow) {
+
+                        float sum = bias_(oc);
+
+                        for (size_t ic = 0; ic < Cin; ++ic) {
+                            for (size_t kh = 0; kh < k; ++kh) {
+                                for (size_t kw = 0; kw < k; ++kw) {
+
+                                    int ih = oh * stride_ + kh - pad_;
+                                    int iw = ow * stride_ + kw - pad_;
+
+                                    if (ih >= 0 && ih < Hin && iw >= 0 && iw < Win) {
+                                        sum += input_(n, ic, ih, iw) * weights_(oc, ic, kh, kw);
+                                    }
+                                }
+                            }
+                        }
+                        output_(n, oc, oh, ow) = sum;
+                    }
+                }
+            }
+        }
+    }
+
+
+protected:  // expect derived classes to access these members
+    size_t in_channels_;
+    size_t out_channels_;
+    size_t kernel_size_;
+    size_t stride_;
+    size_t pad_;
 };
+
+
 
 
 class Linear : public Layer {
